067ebc7f593cf208a4d3a40d8c97a286de629275
Spencer Smith <smithspencer817@gmail.com>
2020-05-05 02:26:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,6 +19,7 @@ def literal_aoh
     KATE_G = { name:  "Kate Gompert", occupation: "None" },
     BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
+  newArr
 end
 
 def aoh_lookup(aoh, row, key)
